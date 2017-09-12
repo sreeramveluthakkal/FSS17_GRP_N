30e57e705d4761d6e2298b8106c6a877d2a0c06b
Sreeram Veluthakkal <sveluth@ncsu.edu>
2017-09-12 19:23:27 -0400

    svm visuals

--- workshop/sveluth/sveluth.py
+++ workshop/sveluth/sveluth.py
@@ -123,3 +123,28 @@ for kernel in ('linear', 'poly', 'rbf', 'sigmoid'):
     svc.fit(iris_X_train, iris_y_train)  
     svc.predict(iris_X_test)
     print iris_y_test
+for k in ['linear', 'poly', 'rbf', 'sigmoid']:
+    print "Plot with kernel= ",k
+
+    # we create an instance of Neighbours Classifier and fit the data.
+    svc = svm.SVC(kernel=k)
+    svc.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z =  svc.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    plt.show()
