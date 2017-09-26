60ddd8f7eff87f0bb1823f5a1b500939051931a5
Aswin Anil Kumar <aswin.sct@gmail.com>
2017-09-26 14:29:34 -0400

    adding test gen file

---
+++ HW3/generate_test.py
@@ -0,0 +1,22 @@
+import random
+
+# sample size
+N = 50
+
+def klass(x):
+    global N
+    if x < 0.2:
+        return 0.2 + random.random()/N
+    elif x < 0.6:
+        return 0.6 + random.random()/N
+    else:
+        return 0.9 + random.random()/N
+
+if __name__ == '__main__':
+    random.seed(random.randint(0,1000))
+    arr = []
+    with open("test_sample.csv","w+") as fp:
+        for i in range(N):
+            x = random.random()
+            y = klass(x)
+            fp.write("%f %f\n"%(x,y))
